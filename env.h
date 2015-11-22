aae49f2f676d7e0ab8101a4d8f847969b105ecb4
mitchlane <mitchlane44@gmail.com>
2015-11-22 11:28:43 -0800

    test

---
+++ env.h
@@ -0,0 +1,13 @@
+#ifndef ENV_H
+#define ENV_H
+
+#include <vector.h>
+
+typedef struct binding {
+  char name[64];
+  void *value;
+}
+
+
+
+#endif
--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include "AST.h"
 #include <stdio.h>
+#include "values.h"
 
 int main(char** args) {
   printf("Hello world.\n");
   return 0;
 }
+
