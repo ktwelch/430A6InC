a2637861788ae6a8c73f6ed11a348fdad011b556
ktwelch <ktwelch@calpoly.edu>
2015-11-22 14:08:38 -0800

    Integrated interp cases and added test cases.

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include "AST.h"
 #include <stdio.h>
+#include <string.h>
 #include "values.h"
+#include "env.h"
 
 // left and right are Values, returns a Value
 void* eval_binop(char* symbol, void* left_void, void* right_void) {
@@ -9,37 +11,37 @@ void* eval_binop(char* symbol, void* left_void, void* right_void) {
   switch(symbol[0]) {
     case '+':
       if (*left == 0 && *right == 0) {
-        return alloc_numV(left[1] + right[1]);
+        return alloc_numV(((numV*) left_void)->num + ((numV*) right_void)->num);
       }
       return NULL;
 
     case '-':
       if (*left == 0 && *right == 0) {
-        return alloc_numV(left[1] - right[1]);
+        return alloc_numV(((numV*) left_void)->num - ((numV*) right_void)->num);
       }
       return NULL;
 
     case '*':
       if (*left == 0 && *right == 0) {
-        return alloc_numV(left[1] * right[1]);
+        return alloc_numV(((numV*) left_void)->num * ((numV*) right_void)->num);
       }
       return NULL;
 
     case '/':
       if (*left == 0 && *right == 0) {
-        if (right[1] == 0.0f)
+        if (((numV*) left_void)->num == 0.0f)
           return NULL;
         else
-          return alloc_numV(left[1] / right[1]);
+          return alloc_numV(((numV*) left_void)->num / ((numV*) right_void)->num);
       }
       return NULL;
 
     case 'e':
       if (left[0] == right[0]) {
         if (left[0] == 0) // two numV's
-          return alloc_boolV(left[1] - right[1] < 0.001f);
+          return alloc_boolV(((numV*) left_void)->num - ((numV*) right_void)->num < 0.001f);
         if (left[0] == 1) // two boolV's
-          return alloc_boolV(left[1] == right[1]);
+          return alloc_boolV(((numV*) left_void)->num == ((numV*) right_void)->num);
         return alloc_boolV(0);
       }
       else
@@ -47,7 +49,7 @@ void* eval_binop(char* symbol, void* left_void, void* right_void) {
 
     case '<':
       if (*left == 0 && *right == 0) {
-        return alloc_numV(left[1] <= right[1]);
+        return alloc_numV(((numV*) left_void)->num <= ((numV*) right_void)->num);
       }
       return NULL;
       
@@ -57,7 +59,8 @@ void* eval_binop(char* symbol, void* left_void, void* right_void) {
 }
 
 // evaluates an ExprC into a Value
-void* interp(void* expr) { // add environment support
+void* interp(void* expr, binding* env) { // add environment support
+  ifC *ifc;
   lamC *lamc;
   binopC *binopc;
   appC *appc;
@@ -66,26 +69,32 @@ void* interp(void* expr) { // add environment support
   void *right_val;
 
   switch(*(int*)expr) {
-    case 0:  // numC
-      return alloc_numV(0);
-
-    case 1:  // boolC
-      return alloc_numV(0);
-
-    case 2:  // idC
-      return alloc_numV(0);
-
-    case 3:  // ifC
-      return alloc_numV(0);
-
+    case 0: //numC
+			return alloc_numV(((struct numC*) expr)->num);
+		case 1: //boolC
+			return alloc_boolV(((struct boolC*) expr)->bool);
+		case 2: ///idC
+			return look_up(((struct idC*) expr)->id, env);
+		case 3:	//ifC
+			ifc = (ifC*)expr;
+			void* cond = interp(ifc->cond, env);
+			if (*(int*)cond == 1) {
+				if (((boolV*) cond)->bool) {
+					return interp(ifc->left, env);
+				} else {
+					return interp(ifc->right, env);
+				}
+			} else {
+				printf("error: ifC conditional was of type: %d \n", *(int*)cond);
+			}
     case 4:  // lamiC
       lamc = (lamC*)expr;  // need environments
       return NULL;
       
     case 5:  // binopC
       binopc = (binopC*)expr;
-      left_val = interp(binopc->left);
-      right_val = interp(binopc->right);
+      left_val = interp(binopc->left, env);
+      right_val = interp(binopc->right, env);
       return eval_binop(binopc->op, left_val, right_val);
       
     case 6:  // appC
@@ -98,7 +107,7 @@ void* interp(void* expr) { // add environment support
 }
 
 char *serialize(void *expr) {
-  void *result = interp(expr);
+  void *result = interp(expr, NULL);
   char *string;
   float num_result;
   int bool_result;
@@ -107,7 +116,7 @@ char *serialize(void *expr) {
   switch (*(int*)result) {
     case 0:
       num_result = ((numV*)result)->num;
-      snprintf(string, 16, "%.3f\n", num_result);
+      snprintf(string, 16, "%.3f", num_result);
       return string;
     case 1:
       bool_result = ((boolV*)result)->bool;
@@ -119,10 +128,25 @@ char *serialize(void *expr) {
 
 int main(char** args) {
   char *string;
+  	binding *env = new_binding();
+	add_binding(&env, "x", alloc_numV(42));
+  
+  printf("interp tests\n");
+	printf("%s \n", (((boolV*) interp(alloc_boolC(1), NULL))->bool == 1) ? "pass" : "fail");
+	printf("%s \n", (((numV*) interp(alloc_numC(-1), NULL))->num == -1) ? "pass" : "fail");
+	printf("%s \n", (((numV*) interp(alloc_idC("x"), env))->num == 42) ? "pass" : "fail");
+	printf("%s \n", (((numV*) interp(alloc_idC("y"), env)) == NULL) ? "pass" : "fail");
+	printf("%s \n", (((numV*) interp(alloc_ifC(alloc_boolC(1), alloc_numC(42), alloc_numC(21)), NULL))->num == 42) ? "pass" : "fail");
 
-  //tests
-  string = serialize(interp(alloc_binopC("+", alloc_numC(5), alloc_numC(3))));
-  printf(string);
+  printf("serialize tests\n");
+  printf("%s \n", strcmp(serialize(interp(alloc_numC(2), NULL)),
+    "2.000") ? "fail" : "pass");
+  printf("%s \n", strcmp(serialize(interp(alloc_binopC("+", alloc_numC(5), alloc_numC(3)), NULL)),
+    "8.000") ? "fail" : "pass");
+  printf("%s \n", strcmp(serialize(interp(alloc_binopC("/", alloc_numC(15), alloc_numC(3)), NULL))),
+    "3.000") ? "fail" : "pass");
+  printf("%s \n", strcmp(serialize(interp(alloc_numC(2), NULL)),
+    "2.000") ? "fail" : "pass");
   
   return 0;
 }
